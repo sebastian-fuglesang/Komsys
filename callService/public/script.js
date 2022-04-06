0007ace13b243a3288329c19e5430f567e874fa2
sebastfu <sebastian.fuglesang@hotmail.com>
2022-04-06 15:24:18 +0200

    Copy of what is already hosted at heroku

--- callService/public/script.js
+++ callService/public/script.js
@@ -1,59 +1,99 @@
-const socket = io('/')
-const videoGrid = document.getElementById('video-grid')
-const myPeer = new Peer(undefined, {
-  host: '/',
-  port: '3001'
-})
-const myVideo = document.createElement('video')
-myVideo.muted = true
+const socket = io();
+const videoGrid = document.getElementById("video-grid");
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+
 const peers = {}
-navigator.mediaDevices.getUserMedia({
-  video: true,
-  audio: true
-}).then(stream => {
-  addVideoStream(myVideo, stream)
 
-  myPeer.on('call', call => {
-    call.answer(stream)
-    const video = document.createElement('video')
+var myPeer = new Peer(undefined, {
+    path: "/peerjs",
+    host: "/",
+    port: "",
+});
 
-    call.on('stream', userVideoStream => {
-      addVideoStream(video, userVideoStream)
+let myVideoStream;
+navigator.mediaDevices
+    .getUserMedia({
+        audio: true,
+        video: true,
     })
-  })
+    .then((stream) => {
+        myVideoStream = stream;
+        addVideoStream(myVideo, stream);
+        myPeer.on("call", (call) => {
+            console.log("call initiated")
+            call.answer(stream);
+            const video = document.createElement("video");
+            call.on("stream", (userVideoStream) => {
+            addVideoStream(video, userVideoStream);
+            });
+        });
+        socket.on("user-connected", (userId) => {
+            console.log("user with userId " + userId + " connected.")
+            connectToNewUser(userId, stream);
+            });
+        });
 
-  socket.on('user-connected', userId => {
-    setTimeout(connectToNewUser,3000, userId, stream) //Rydd opp her. For at stream skal komme i tide
-  })
-})
+
+myPeer.on("open", (id) => {
+    socket.emit("join-room", ROOM_ID, id);
+    console.log("you have the following roomId " + ROOM_ID)
+});
 
 socket.on('user-disconnected', userId => {
-  if (peers[userId]) peers[userId].close()
-})
-
-myPeer.on('open', id => {
-  socket.emit('join-room', ROOM_ID, id)
-})
-
-function connectToNewUser(userId, stream) {
-  console.log("called connect to new user")
-  const call = myPeer.call(userId, stream)
-  const video = document.createElement('video')
-  call.on('stream', userVideoStream => {
-    addVideoStream(video, userVideoStream)
+    console.log("user with userId " + userId + " disconnected.")
+    if (peers[userId]) peers[userId].close()
   })
-  call.on('close', () => {
-    video.remove()
-  })
-
-  peers[userId] = call
-}
 
 function addVideoStream(video, stream) {
-  console.log("add video stream called")
-  video.srcObject = stream
-  video.addEventListener('loadedmetadata', () => {
-    video.play()
-  })
-  videoGrid.append(video)
-}
+    video.srcObject = stream;
+    video.addEventListener("loadedmetadata", () => {
+    video.play();
+    });
+    videoGrid.append(video);
+};
+
+function connectToNewUser(userId, stream) {
+    const call = myPeer.call(userId, stream);
+    const video = document.createElement("video");
+    call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+    });
+
+    call.on('close', () => {
+        console.log("call closed and video should be removed")
+        video.remove()
+      })
+      peers[userId] = call
+};
+
+  //Chat stuff
+
+  var form = document.querySelector('form');
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    var input = document.querySelector('#message-input');
+    var text = input.value;
+    socket.emit('messageSent', text);
+    input.value = '';
+  });
+  
+socket.on('messageReceived', text => {
+    console.log("message received")
+    receiveNewMessage(text)
+});
+
+function receiveNewMessage(newMessageContent) {
+    if (!newMessageContent){
+      return;
+    }
+    console.log(newMessageContent)
+      
+    var chatMessageContainer = document.getElementById("chat-message-container");
+    var newMessage = document.createElement("p");
+    newMessage.innerText = newMessageContent;
+    chatMessageContainer.appendChild(newMessage);
+    var seperator = document.createElement('br');
+    chatMessageContainer.appendChild(seperator);
+    }
+      
