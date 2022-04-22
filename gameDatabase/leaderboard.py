49b9e8aa314898189c5aa51c2031a581074c359f
nilotuv <nilsotu@stud.ntnu.no>
2022-04-22 13:25:13 +0200

    motion detection updated, leaderboard, and update to readfromDB

--- gameDatabase/leaderboard.py
+++ gameDatabase/leaderboard.py
@@ -1,4 +1,5 @@
 from appJar import gui
+from writeToDatabase import db
 
 def leaderboard():
     app = gui()
@@ -7,25 +8,26 @@ def leaderboard():
         app.stop()
 
     #fetch from db
+    data = db.readFromDatabase()
+    print(data)
 
     #testdata for now
-    testdata = [['Nils', 2], ['Olav', 3], ['Tuv', 0]]
-    testdata = sorted(testdata, key = lambda x: x[1], reverse=True)
+    #testdata = [['Nils', 2], ['Olav', 3], ['Tuv', 0]]
+    #testdata = sorted(testdata, key = lambda x: x[1], reverse=True)
 
     #app setup
     app.setTitle('Leaderboard')
-    app.addLabel('leader', 'The leader is: Nils')
+    app.addLabel('leader', 'The leader is: ' + data[0][0])
     app.addTable('table', [['Name', 'Wins']])
     
     #adds rows in table
-    for user in testdata:
+    for user in data:
         app.addTableRow('table', user)
 
     app.addButton('leave', button_close)
     
-    
-    
     app.go()
+
 
 
 leaderboard()
